487cf0860ffa3c861c9c49dbe5971931a1b52bf7
Luke Forehand <lforehan@amfam.com>
2024-08-22 17:12:33 -0500

    docs

--- api/queue.go
+++ api/queue.go
@@ -1,3 +1,6 @@
+/*
+queue.go contains methods for queuing tasks
+*/
 package api
 
 import (
@@ -9,6 +12,8 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+// ReceiptQueue is a simple in memory key/value queue implementation
+// for id/Receipt
 type ReceiptQueue struct {
 	// context
 	ctx context.Context
@@ -18,21 +23,7 @@ type ReceiptQueue struct {
 	client *redis.Client
 }
 
-func (r ReceiptQueue) Enqueue(values ...string) error {
-	value, _ := json.Marshal(values)
-	return r.client.LPush(r.ctx, r.name, value).Err()
-}
-
-func (r ReceiptQueue) Dequeue() ([]string, error) {
-	data, err := r.client.BRPop(r.ctx, 0, r.name).Result()
-	if err != nil {
-		return []string{}, err
-	}
-	var values []string
-	err = json.Unmarshal([]byte(data[1]), &values)
-	return values, err
-}
-
+// NewReceiptQueue initializes ReceiptQueue with defaults
 func NewReceiptQueue(name string) ReceiptQueue {
 	return ReceiptQueue{
 		ctx:  context.Background(),
@@ -41,4 +32,24 @@ func NewReceiptQueue(name string) ReceiptQueue {
 			Addr: fmt.Sprintf("%s:6379", os.Getenv("REDIS_HOST")),
 		}),
 	}
+}
+
+// Enqueue puts a id/Receipt to the back of the queue
+// id: the uuid string associated with a Receipt
+// Returns: the Receipt for the given id
+func (r ReceiptQueue) Enqueue(id string, receipt Receipt) error {
+	value, _ := json.Marshal(&receipt)
+	return r.client.LPush(r.ctx, r.name, value).Err()
+}
+
+// Dequeue gets a id/Receipt from the front of the queue
+// Returns: slice of id/Receipt serialized as a string
+func (r ReceiptQueue) Dequeue() ([]string, error) {
+	data, err := r.client.BRPop(r.ctx, 0, r.name).Result()
+	if err != nil {
+		return []string{}, err
+	}
+	var values []string
+	err = json.Unmarshal([]byte(data[1]), &values)
+	return values, err
 }
